06d055cc4a6e340d79540fda30039f030944a592
TrevorLawyer <trevorlawyer@gmail.com>
2017-10-28 20:10:12 -0500

    Test

--- app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+    //This is a test for pushing and pulling. Delete.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
